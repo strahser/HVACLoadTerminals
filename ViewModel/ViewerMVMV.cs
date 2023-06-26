8d6d517f86b1035b6aa66686c8553d0995f1156f
strahser <ssg-81@yandex.ru>
2023-06-26 20:00:01 +0300

    test. не видит datacontext

---
+++ ViewModel/ViewerMVMV.cs
@@ -0,0 +1,42 @@
+﻿using HVACLoadTerminals.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+namespace HVACLoadTerminals.ViewModel
+{
+    public class ViewerMVMV : ViewModelBase
+    {
+        public Document document;
+
+
+        private Dictionary<string, BuiltInCategory> categoryes = new Dictionary<string, BuiltInCategory>()
+                {
+                    { "OST_DuctTerminal", BuiltInCategory.OST_DuctTerminal},
+                    { "OST_MechanicalEquipment", BuiltInCategory.OST_MechanicalEquipment},
+                };
+        private Dictionary<string, BuiltInCategory> _categoryes;
+        private string _selectedCategory;
+        public string SelectedCategory { get { return _selectedCategory; } set {
+                _selectedCategory = value;
+                OnPropertyChanged("SelectedCategory");
+            }
+        }
+        private BuiltInCategory _selectedBINCategory;
+        public Dictionary<string, BuiltInCategory> Categoryes
+        {
+            get { return categoryes; }
+            set
+            {
+                _categoryes = value;
+                OnPropertyChanged("Categoryes");
+                _selectedBINCategory = categoryes[SelectedCategory];
+            }
+        }
+        public BuiltInCategory SelectedBINCategory { get { return _selectedBINCategory; } set { } }
+
+    }
+}
--- Viewer.xaml.cs
+++ Viewer.xaml.cs
@@ -14,58 +14,24 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using HVACLoadTerminals.ViewModel;
 namespace HVACLoadTerminals
 {
     /// <summary>
     /// Логика взаимодействия для UserControl1.xaml
     /// </summary>
     public partial class Viewer : Window
+
     {
         public Document document;
-        private Dictionary<string, BuiltInCategory> categoryes = new Dictionary<string, BuiltInCategory>()
-                {
-                    { "OST_DuctTerminal", BuiltInCategory.OST_DuctTerminal},
-                    { "OST_MechanicalEquipment", BuiltInCategory.OST_MechanicalEquipment},
-                };
-
         public Viewer(Document doc)
         {
-            // assign value to field
             document = doc;
+            ViewerMVMV viewer = new ViewerMVMV();
             InitializeComponent();
+            DataContext = viewer;
 
-            CategorySelectedLabelComboBox.ItemsSource = categoryes.Keys;
-            CategorySelectedLabelComboBox.SelectedIndex=0;
 
-        }
-
-        private void familyTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-            familySelectedLabel.Text = familyTypeComboBox.SelectedItem?.ToString();
-
-        }
-
-        private void CategorySelectedLabelComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            BuiltInCategory selectedCategory = categoryes[CategorySelectedLabelComboBox.SelectedItem.ToString()];
-            BuiltInCategory BIC = (BuiltInCategory)BuiltInCategory.Parse(typeof(BuiltInCategory), CategorySelectedLabelComboBox.SelectedItem?.ToString());
-            IList < Element > category = new FilteredElementCollector(document)
-                .WherePasses(new ElementCategoryFilter(BIC))
-                .WhereElementIsElementType()
-                .ToList();
-            ListView1.ItemsSource = category.Select(cat=>cat.Name).ToList();
-            
-            Dictionary<string, List<FamilySymbol>> winFamilyTypes = CollectorQuery.FindFamilyTypes(document, selectedCategory);
-            Dictionary<string, List<string>> winFamilyNames = new Dictionary<string, List<string>>();
-            
-            familyTypeComboBox.ItemsSource = winFamilyTypes.Keys;
-            familyTypeComboBox.SelectedIndex = 0;
-            foreach (KeyValuePair<string, List<FamilySymbol>> items in winFamilyTypes)
-            {
-                winFamilyNames[items.Key] = items.Value.Select(el => el.Name).ToList();
-            }
-            treeView1.DataContext = winFamilyNames;
         }
     }
 }
