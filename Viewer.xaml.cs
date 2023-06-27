e51da89ab31a58e7c742d0adaad291c6aa78a86a
strahser <ssg-81@yandex.ru>
2023-06-27 19:59:22 +0300

    cascad combobox рабочий. создал три каскадных категория, семейство, параметр

--- Viewer.xaml.cs
+++ Viewer.xaml.cs
@@ -27,59 +27,70 @@ namespace HVACLoadTerminals
                     { "OST_DuctTerminal", BuiltInCategory.OST_DuctTerminal},
                     { "OST_MechanicalEquipment", BuiltInCategory.OST_MechanicalEquipment},
                 };
-
+        public Dictionary<string, List<FamilySymbol>> winFamilyTypes { get; set; }
         public Viewer(Document doc)
         {
             // assign value to field
             document = doc;
             InitializeComponent();
-
             CategorySelectedLabelComboBox.ItemsSource = categoryes.Keys;
-            CategorySelectedLabelComboBox.SelectedIndex=0;
+            CategorySelectedLabelComboBox.SelectedIndex = 0;
+
 
         }
 
-        private void familyTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
-            familySelectedLabel.Text = familyTypeComboBox.SelectedItem?.ToString();
-
-        }
 
         private void CategorySelectedLabelComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            BuiltInCategory selectedCategory = categoryes[CategorySelectedLabelComboBox.SelectedItem.ToString()];
+            BuiltInCategory selectedCategory = categoryes[CategorySelectedLabelComboBox.SelectedItem?.ToString()];
             BuiltInCategory BIC = (BuiltInCategory)BuiltInCategory.Parse(typeof(BuiltInCategory), CategorySelectedLabelComboBox.SelectedItem?.ToString());
-            IList < Element > category = new FilteredElementCollector(document)
+            IList<Element> category = new FilteredElementCollector(document)
                 .WherePasses(new ElementCategoryFilter(BIC))
                 .WhereElementIsElementType()
                 .ToList();
-            
-            Dictionary<string, List<FamilySymbol>> winFamilyTypes = CollectorQuery.FindFamilyTypes(document, selectedCategory);
-            Dictionary<string, List<string>> winFamilyNames = new Dictionary<string, List<string>>();
-            
+            winFamilyTypes = CollectorQuery.FindFamilyTypes(document, selectedCategory);
+           
+
             familyTypeComboBox.ItemsSource = winFamilyTypes.Keys;
+            //familyTypeComboBox.SelectedValue = "value";
+            //familyTypeComboBox.DisplayMemberPath = "Key";
             familyTypeComboBox.SelectedIndex = 0;
-            var familyType = winFamilyTypes[familyTypeComboBox.SelectedItem?.ToString()].FirstOrDefault();
-            List<string> parametrList = CollectorQuery.GetParameters(familyType)
-                .Where(p => familyType.LookupParameter(p).StorageType== StorageType.Double).ToList();
-            foreach (string p in parametrList)
-            {             
-                var storeType = familyType.LookupParameter(p).StorageType;
-                if (storeType == StorageType.Double)
-                {
-                    familyType.LookupParameter(p).AsDouble();
-                }
-            }
-            ListView1.ItemsSource = winFamilyTypes[familyTypeComboBox.SelectedItem?.ToString()].Select(cat=>cat.Name).ToList();
-            ListViewParametr.ItemsSource = parametrList;
-            ListViewParametr.SelectedIndex = 1;
-            ViewParametrSelectedLabel.Text = ListViewParametr.SelectedItems[0].ToString();
+            Dictionary<string, List<string>> winFamilyNames = new Dictionary<string, List<string>>();
             foreach (KeyValuePair<string, List<FamilySymbol>> items in winFamilyTypes)
             {
                 winFamilyNames[items.Key] = items.Value.Select(el => el.Name).ToList();
             }
             treeView1.DataContext = winFamilyNames;
+        }
+    
+        private void FamilyTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        {
+            if (familyTypeComboBox.SelectedItem != null)
+            {
+                Element familyType = winFamilyTypes[familyTypeComboBox.SelectedItem?.ToString()].FirstOrDefault();
+                if (familyType != null)
+                {
+                    List<string> parametrList = CollectorQuery.GetParameters(familyType)
+                        .Where(p => familyType.LookupParameter(p).StorageType == StorageType.Double).ToList();
+                    foreach (string p in parametrList)
+                    {
+                        var storeType = familyType.LookupParameter(p).StorageType;
+                        if (storeType == StorageType.Double)
+                        {
+                            familyType.LookupParameter(p).AsDouble();
+                        }
+                    }
+                    ParameterChooseComboBox.ItemsSource = parametrList;
+                    ParameterChooseComboBox.SelectedIndex = 0;
+                }
+            }
+        }
+        private void ParameterChooseComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
         }
     }
 }
