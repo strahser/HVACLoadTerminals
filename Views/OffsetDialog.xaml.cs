acac3351b4102a500d4e455a7527362714de217f
strahser <ssg-81@yandex.ru>
2024-09-22 10:29:54 +0300

    первый опыт

--- Views/OffsetDialog.xaml.cs
+++ Views/OffsetDialog.xaml.cs
@@ -8,11 +8,14 @@ namespace HVACLoadTerminals.Views
 {
     public partial class OffsetDialog : Window
     {
+
         public OffsetDialog(SQLiteConnection connection, SpaceBoundary _spaceBoundary)
         {
             InitializeComponent();
             DataContext = new OffsetDialogViewModel(connection, _spaceBoundary);
+            
         }
+
     }
 
 }
